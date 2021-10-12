244efe2b2995518061f5d827af0a13345fb22101
zetarop <arkupa199888@gmail.com>
2021-10-12 16:59:50 +0530

    Update min_swap_to_balance_string.cpp

--- min_swap_to_balance_string.cpp
+++ min_swap_to_balance_string.cpp
@@ -1,3 +1,12 @@
+/*
+
+You are given a string S of 2N characters consisting of N ‘[‘ brackets and N ‘]’ brackets. 
+A string is considered balanced if it can be represented in the for S2[S1] where S1 and S2 are balanced strings. 
+We can make an unbalanced string balanced by swapping adjacent characters. Calculate the minimum number of swaps necessary to make a string balanced.
+Note - Strings S1 and S2 can be empty.
+
+*/
+
 #include <iostream>
 #include<string>
 #include<vector>
